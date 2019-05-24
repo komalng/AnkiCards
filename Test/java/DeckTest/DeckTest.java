98551b58800e29a0193a81ff25b93b4401aea2a3
komal.bhatt <komal18@navgurukul.org>
2019-05-24 20:13:36 +0530

    Add.Testcase for Deck class.

--- Test/java/DeckTest/DeckTest.java
+++ Test/java/DeckTest/DeckTest.java
@@ -5,5 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DeckTest {
-
+    @Test public void shouldCreateDeck(){
+        Deck name = new Deck("Programming");
+        assertEquals("Programming",name.name);
+    }
 }
