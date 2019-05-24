0c02b65647a71601bbf54b120bb3aa475a0132ac
komal.bhatt <komal18@navgurukul.org>
2019-05-24 15:48:21 +0530

    Add. Cards class.

---
+++ main/java/logic/Cards.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class Cards {
+    public String frontCards;
+    public String backCards;
+
+    public Cards(String frontCards,String backCards){
+        this.frontCards = frontCards;
+        this.backCards = backCards;
+    }
+}
