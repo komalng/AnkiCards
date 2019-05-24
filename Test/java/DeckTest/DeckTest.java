da5d6b82c2273a265917d20d56f13db4f0701478
komal.bhatt <komal18@navgurukul.org>
2019-05-24 12:29:11 +0530

    first commit

---
+++ Test/java/DeckTest/DeckTest.java
@@ -0,0 +1,9 @@
+package DeckTest;
+import logic.Deck;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+
+}
---
+++ main/java/logic/Deck.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class Deck {
+    public String name;
+
+    public Deck(String createName){
+        this.name = createName;
+    }
+}
