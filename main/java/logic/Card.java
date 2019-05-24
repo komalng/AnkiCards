cc9e80bdfab11727896587723a6ebc58f44537ab
komal.bhatt <komal18@navgurukul.org>
2019-05-24 18:31:26 +0530

    refactor.card to Card.

--- main/java/logic/Card.java
+++ main/java/logic/Card.java
@@ -1,10 +1,10 @@
 package logic;
 
-public class card {
+public class Card {
     public String front;
     public String back;
 
-    public card(String front, String back){
+    public Card(String front, String back){
         this.front = front;
         this.back = back;
     }
