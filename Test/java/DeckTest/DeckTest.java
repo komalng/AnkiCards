f53782f228815d2f2dcbb9fed87b393526a8e3fc
komal.bhatt <komal18@navgurukul.org>
2019-05-27 12:28:51 +0530

    Refactor.shouldAddCarddInCards to shouldAddCardInCards.

--- Test/java/DeckTest/DeckTest.java
+++ Test/java/DeckTest/DeckTest.java
@@ -17,7 +17,7 @@ public class DeckTest {
         Card frontBack = new Card("ssh","Secure Shell");
         assertEquals("ssh,Secure Shell",frontBack.toString());
     }
-    @Test public void shouldAddCarddInCards(){
+    @Test public void shouldAddCardInCards(){
         Deck deck = new Deck("Programming");
         Card createCard = new Card("ssh","Secure Shell");
         List<Card> expected = new ArrayList<>();
